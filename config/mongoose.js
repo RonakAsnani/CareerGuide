cf6d0a8cf71c8da02ea2c8d9d7f926916f7604f6
Ronak <ronakasnani5@gmail.com>
2023-11-09 16:05:42 +0530

    changes for hosting

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/careerguide_development');
+mongoose.connect(
+  "mongodb+srv://ronakasnani5:2RF8V3Z1dr6j0gmQ@cluster0.m5abuce.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
-
 
 module.exports = db;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,176 +1,169 @@
-const User = require('../models/users');
+const User = require("../models/users");
 
-
-
-module.exports.profile = function(req,res){
-    return res.render('dashboard/dashboard',{
-        title: "User Profile"
-       
-    })
-}
-module.exports.mentorProfile = function(req,res){
-    return res.render('mentor_profile',{
-        title: "Mentor Profile"
-    })
-}
+module.exports.profile = function (req, res) {
+  return res.render("dashboard/dashboard", {
+    title: "User Profile",
+  });
+};
+module.exports.mentorProfile = function (req, res) {
+  return res.render("mentor_profile", {
+    title: "Mentor Profile",
+  });
+};
 
 // module.exports.tutor=function(req,res){
 //     return res.render('dashboard/mentor1',{
 //         title:"Mentors"
-        
+
 //     })
 // }
-module.exports.detail=function(req,res){
-    return res.render('dashboard/student',{
-        title:"Detail"
-        
-    })
-}
-module.exports.testRules = function(req,res){
-    return res.render('test-rules',{
-        title: "rules"
-    })
-}
-module.exports.showInfo = function(req,res){
-    return res.render('mentor-info',{
-        title: "info"
-    })
-}
-
-module.exports.signUp = function(req,res){
-
-
-    return res.render('signup_copy',{
-        title: "CareerGuide | Sign Up"
-    })
+module.exports.detail = function (req, res) {
+  return res.render("dashboard/student", {
+    title: "Detail",
+  });
+};
+module.exports.testRules = function (req, res) {
+  return res.render("test-rules", {
+    title: "rules",
+  });
+};
+module.exports.showInfo = function (req, res) {
+  return res.render("mentor-info", {
+    title: "info",
+  });
 };
 
-module.exports.signIn = function(req,res){
+module.exports.signUp = function (req, res) {
+  return res.render("signup_copy", {
+    title: "CareerGuide | Sign Up",
+  });
+};
 
-    if(req.isAuthenticated()){
-       // console.log(res.locals);
-        if(res.locals.user.field == "student"){
-            return res.redirect('/users/profile');
-        }
-        if(res.locals.user.field == "mentor"){
-            return res.redirect('/users/mentor_profile');
-        }
-        
+module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    // console.log(res.locals);
+    if (res.locals.user.field == "student") {
+      return res.redirect("/users/detail");
     }
-    return res.render('sign-in',{
-        title: "CareerGuide | Sign In"
-    })
-}
-
-
+    if (res.locals.user.field == "mentor") {
+      return res.redirect("/users/mentor_profile");
+    }
+  }
+  return res.render("sign-in", {
+    title: "CareerGuide | Sign In",
+  });
+};
 
 // // get sign up data
-module.exports.create = function(req,res){
-    if(req.body.password != req.body.confirm_password){
-        
-        //return res.redirect('back');
-        return res.render('signup_copy',{
-            title: "match"
-        })
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    //return res.redirect('back');
+    return res.render("signup_copy", {
+      title: "match",
+    });
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
     }
-    User.findOne({email : req.body.email},function(err,user){
-        if(err){
-            console.log("error in finding user in signing up"); 
-            return;
-        }
-        if(!user){
-            User.create(req.body,function(err,user){
-                if(err){
-                    console.log("error in creating user in signing up"); 
-                    return;
-                }
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.render('signup_copy',{
-                title: "exist"
-            })
-        }
-
-    
-    })
-    //console.log(req.body);
-
-}
-
-module.exports.verifyMentor = function(req,res){
-    User.findOneAndUpdate({email: res.locals.user.email}, {$set:{verified:"true"}}, {new: true}, (err, doc) => {
+    if (!user) {
+      User.create(req.body, function (err, user) {
         if (err) {
-            console.log("Something wrong when updating data!");
+          console.log("error in creating user in signing up");
+          return;
         }
-    
-        //console.log(doc);
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.render("signup_copy", {
+        title: "exist",
+      });
+    }
+  });
+  //console.log(req.body);
+};
+
+module.exports.verifyMentor = function (req, res) {
+  User.findOneAndUpdate(
+    { email: res.locals.user.email },
+    { $set: { verified: "true" } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        console.log("Something wrong when updating data!");
+      }
+
+      //console.log(doc);
+    }
+  );
+  // console.log(res.locals.user);
+  return res.redirect("/users/mentor_profile");
+};
+
+module.exports.summary = function (req, res) {
+  //console.log(req.body.mentorsummary);
+  User.findOneAndUpdate(
+    { email: res.locals.user.email },
+    { $set: { summary: req.body.mentorsummary } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        console.log("Something wrong when updating data!");
+      }
+    }
+  );
+  return res.redirect("/users/mentor-info");
+};
+
+module.exports.studentDisplay = function (req, res) {
+  User.find({ field: "student" }, function (err, users) {
+    if (err) {
+      console.log("Something wrong when updating data!");
+    }
+    const studentList = users;
+    return res.render("student-display", {
+      title: "display",
+      student_list: studentList,
     });
-    // console.log(res.locals.user);
-    return res.redirect('/users/mentor_profile');
-}
+  });
+};
 
-module.exports.summary = function(req,res){
-    //console.log(req.body.mentorsummary);
-    User.findOneAndUpdate({email:res.locals.user.email},{$set:{summary: req.body.mentorsummary}}, {new: true}, (err, doc) => {
-        if (err) {
-            console.log("Something wrong when updating data!");
-        }
-        
-    })
-    return res.redirect('/users/mentor-info')
-}
+module.exports.removeInterest = function (req, res) {
+  //console.log(res.locals.user);
+  // res.locals.user.interest == "none";
+  // console.log(res.locals.user);
+  User.findOneAndUpdate(
+    { email: res.locals.user.email },
+    { $set: { interest: "none" } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        console.log("Something wrong when updating data!");
+      }
 
-module.exports.studentDisplay = function(req,res){
-    User.find({field: "student"},function(err,users){
-        if (err) {
-            console.log("Something wrong when updating data!");
-        }
-        const studentList = users;
-        return res.render('student-display',{
-            title:"display",
-            student_list : studentList
-        })
-        
-    })
-}
+      // console.log(doc);
+    }
+  );
+  // console.log(res.locals.user);
+  return res.redirect("/users/mentor_profile");
+};
 
+module.exports.createSession = function (req, res) {
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (user.field == "student") {
+      return res.redirect("/dashboard/start");
+    }
+    if (user.field == "mentor") {
+      return res.redirect("/users/mentor_profile");
+    }
+  });
 
+  //return res.redirect('/users/sign-in');
+};
 
-module.exports.removeInterest = function(req,res){
-    //console.log(res.locals.user);
-    // res.locals.user.interest == "none";
-    // console.log(res.locals.user);
-    User.findOneAndUpdate({email: res.locals.user.email}, {$set:{interest:"none"}}, {new: true}, (err, doc) => {
-        if (err) {
-            console.log("Something wrong when updating data!");
-        }
-    
-       // console.log(doc);
-    });
-    // console.log(res.locals.user);
-    return res.redirect('/users/mentor_profile');
-}
-
-
-
-
-module.exports.createSession = function(req,res){
-    User.findOne({email:req.body.email},function(err,user){
-        if(user.field == "student"){
-            return res.redirect('/dashboard/start');
-        }
-        if(user.field == "mentor"){
-            return res.redirect('/users/mentor_profile');
-        }
-    })
-    
-    //return res.redirect('/users/sign-in');
-    
-}
-
-module.exports.destroySession = function(req,res){
-    req.logout();
-    return res.redirect('/');
-}
+module.exports.destroySession = function (req, res) {
+  req.logout();
+  return res.redirect("/");
+};
